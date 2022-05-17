dd1a8620be99c591bd03a1624179aa73ae65a2a5
oaitbena <omar.ait-benaissa@capgemini.com>
2022-05-17 17:39:01 +0200

    [Leadconsulting] : KATA US-1 CODE

--- kata-banking-oab/src/main/java/Model/Account.java
+++ kata-banking-oab/src/main/java/Model/Account.java
@@ -1,6 +1,9 @@
 package Model;
 
+import transvers.ConstantesBanking;
+
 import java.io.Serializable;
+import java.util.*;
 
 
 public class Account implements Serializable {
@@ -17,6 +20,10 @@ public class Account implements Serializable {
 
     public void deposit(Integer amount) throws BankingException{
 
+        if(Objects.isNull(amount)){
+            throw new BankingException(ConstantesBanking.MSG_VEUILLEZ_SAISIR_MONTANT);
+        }
+        this.balance+=amount;
     }
 
 
