2f035cd9ecc70547297f0e6ced96b6060e28fe96
oaitbena <omar.ait-benaissa@capgemini.com>
2022-05-17 17:44:55 +0200

    [Leadconsulting] : KATA US-2 CODE

--- kata-banking-oab/src/main/java/Model/Account.java
+++ kata-banking-oab/src/main/java/Model/Account.java
@@ -3,7 +3,7 @@ package Model;
 import transvers.ConstantesBanking;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.*;
 
 
 public class Account implements Serializable {
@@ -11,21 +11,27 @@ public class Account implements Serializable {
     private int balance;
 
 
+
     public Account(int initialBalance) {
         balance = initialBalance;
     }
 
-    public void deposit(Integer amount) throws BankingException {
+    public void deposit(Integer amount) throws BankingException{
 
-        if (Objects.isNull(amount)) {
+        if(Objects.isNull(amount)){
             throw new BankingException(ConstantesBanking.MSG_VEUILLEZ_SAISIR_MONTANT);
         }
-        this.balance += amount;
+        this.balance+=amount;
     }
 
     public void withdraw(int amount) throws BankingException {
-
-    }
+        if(Objects.isNull(amount)){
+            throw new BankingException(ConstantesBanking.MSG_VEUILLEZ_SAISIR_MONTANT);
+        }else if(amount >this.balance){
+            throw new BankingException(ConstantesBanking.ERR_MONTANT_DEMANDE_SUP_SOLDE);
+        }
+        this.balance-=amount;
+          }
 
 
     /**
